8c49be229e1c3f9df56c7f2b38d65a9639300aa5
DKendle <dina.kendle@gmail.com>
2022-01-12 19:22:05 -0500

    Deletes App.test file and adds fxn to render Home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import Home from './components/Home';
 
 function App() {
+
+
+//Renders Home Component
+  function showComponent(){
+
+  }
+  
+//Handles click on Enter btn
+  function handleClick(event){
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1>Welcome Traveler</h1>
+        <button onClick={handleClick}>Enter</button>
       </header>
     </div>
   );
