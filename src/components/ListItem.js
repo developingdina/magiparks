72d8d235b4d24acbd5460d0f332ff080d5505b52
DKendle <dina.kendle@gmail.com>
2022-02-07 12:49:21 -0500

    Adds a desc to half of the parks. Adds add button to each of the listings.

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import Button from './Button'
 export default function ListItem({title, desc}) {
+
+
+
+    function handleClick(){
+        console.log("clicked")
+    }
 
     return (
         <div style={{border: "2px solid palevioletred", margin: "0 1em", padding: "0.25em 1em"}} >
             <h3 >{title}</h3>
             <h4>{desc}</h4>
-            <text></text>
+            <Button onClick={handleClick}>Add</Button>
         </div>
     )
 }
