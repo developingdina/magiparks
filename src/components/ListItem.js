dafab0c164bef571facff744482a76fe94beb03c
DKendle <dina.kendle@gmail.com>
2022-02-09 15:09:51 -0500

    Adds functioning remove button from list of recommended parks. Adds new prop to send remove function to list item

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import Button from './Button'
-export default function ListItem({title, desc, id}) {
+export default function ListItem({title, desc, id, delFunction}) {
 
 
 
  function deletePark(id){
-    console.log(id)
+    fetch(`http://localhost:3000/parks/${id}`, {
+        method: "DELETE"
+    }).then((resp) => resp.json())
+    .then((data) =>{
+        console.log(data)
+        delFunction(id)
+    })
  }
+ 
     return (
         <div style={{border: "2px solid palevioletred", margin: "0 1em", padding: "0.25em 1em"}} >
             <h3 >{title}</h3>
@@ -20,3 +27,16 @@ export default function ListItem({title, desc, id}) {
 //binds specific park to that instance of a button at that time...
 //Also not constantly calling the function with ()
 //onClick={() => deletePark()} vs onClick={deletePark} vs onClick={deletePark()}
+
+//
+//function deletePark(id){
+////fetch(`http://localhost:3000/parks/${id}`,/{
+///     method: "DELETE"
+ //   }).then((resp) => resp.json())
+ //   .then((data) =>{
+ //       alert(`Park ${data} has been deleted`)
+//    })
+// }
+
+//the above will delete the items, but you will need
+//to refresh to show the result
--- src/components/ParkList.js
+++ src/components/ParkList.js
@@ -7,22 +7,25 @@ export default function ParkList(props) {
 const [card, setCard] = useState([])
 
 useEffect(()=> {
+    getParks()
+  }, [])
+  
+
+  function getParks(id){
     fetch("http://localhost:3000/parks")
     .then((resp) => resp.json())
     .then((data) => {
         setCard(data)
         console.log(card)
     })
-  }, [])
-  
-
+  }
 
     return (
         <div>
             <h1>Other National Parks</h1>
             <ul>
                 {card.map((obj) => (
-                    <ListItem key={obj.title} title={obj.title} desc={obj.desc} id={obj.id}/>
+                    <ListItem key={obj.title} title={obj.title} desc={obj.desc} id={obj.id} delFunction={() => getParks(obj.id)}/>
                 ))}
             </ul>
         </div>
