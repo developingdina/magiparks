dde8aebc491944a6058c6f954a2542f33e430cad
DKendle <dina.kendle@gmail.com>
2022-03-17 20:22:16 -0400

    Adds an error page for user routing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Home from './components/Home';
 import ParkList from './components/ParkList';
 import Navbar from './components/Navbar';
-import { Routes, Route, NavLink } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 import Welcome from './components/Welcome';
 import { About } from './components/About';
 import NewPark from './components/NewPark';
@@ -22,6 +22,7 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/parks/new" element={<NewPark/>}/>
         <Route exact path="/" element={<Welcome />}/>
+        <Route path='*' element={<ErrorPage />} />
       </Routes>
     </div>
   );
---
+++ src/components/ErrorPage.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export default const ErrorPage = () => {
+    <div>Uh Oh....let's get you on your way. Try clicking one of the buttons at the top of the page.</div>
+
+}
